8bb69a85ea8eb78315586b702a4ef87b85a3a9bd
Sean Pletan <avrptl1453@gmail.com>
2026-03-04 12:54:43 -0600

    SMALL stype update + finished about me text

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,10 +1,11 @@
 export default function getAboutMePage() {
      return `
-     <h1>Hi, I'm Sean</h1>
+     <h1>About Me</h1>
      <p>I am a developer who is a new graduate of the University of Houston. My work sits at the intersection of real-time and embedded systems, traditional fullstack web development, and intentional, optimized systems development. Whether implementing an EDF scheduler in FreeRTOS, building GPU-accelerated WebGL simulations, or developing an AI intrusion detection system, I focus on performance, correctness, and resource-aware design.
      </br></br>
-     I’m drawn to unique projects with clear intent. This portfolio itself was an exercise in rethinking the conventional developer website: designing something that feels open and exploratory rather than purely technical. Inspired by time spent outdoors, I wanted the experience to feel expansive and light while still grounded in familiar interaction patterns.
+     I’m drawn to unique projects with clear intent. This portfolio itself was an exercise in rethinking the conventional developer website: designing something that felt open and exploratory rather than purely technical. Inspired by time spent outdoors, I wanted the experience to feel expansive and light while still grounded in familiar interaction patterns.
      </br></br>
-     
+     In my free time, I enjoy working on my NB Mazda Miata, playing Dungeons and Dragons, and hiking. Feel free to check out my media library where I post reviews of the things I enjoy.
+
      </p>`;
 }
